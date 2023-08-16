f88500478f42354acf956bcba6ee4280993f73e8
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2023-08-16 11:42:37 +0600

    Add expected and actual detail provider for assertion

---
+++ JsonSchema/RelogicLabs/JsonSchema/Message/ActualDetail.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics.CodeAnalysis;
+using RelogicLabs.JsonSchema.Tree;
+using RelogicLabs.JsonSchema.Types;
+using RelogicLabs.JsonSchema.Utilities;
+
+namespace RelogicLabs.JsonSchema.Message;
+
+public class ActualDetail : CommonDetail<ActualDetail>
+{
+    [SetsRequiredMembers]
+    public ActualDetail(Context context, string message) 
+        : base(context, message) { }
+    
+    [SetsRequiredMembers]
+    public ActualDetail(JNode node, string message) 
+        : base(node, message) { }
+
+    internal static ActualDetail AsValueMismatch(JNode node)
+        => new(node.Context, node.ToOutline().ToString("found "));
+    
+    internal static ActualDetail AsPropertyNotFound(JNode node, JProperty property) 
+        => new(node.Context, $"not found property key \"{property.Key}\"");
+    
+    internal static ActualDetail AsUnknownProperty(JProperty property)
+        => new(property.Context, $"property found {{{property.ToOutline()}}}");
+    
+    internal static ActualDetail AsArrayElementNotFound(JArray array, int index)
+        => new(array.Context, $"not found");
+    
+    internal static ActualDetail AsInvalidFunction(JNode node) 
+        => new(node, $"applied on non-composite type {GetType(node)}");
+    
+    internal static ActualDetail AsInvalidDataType(JNode node) 
+        => new(node, $"applied on non-composite type {GetType(node)}");
+
+    internal static ActualDetail AsPropertyOrderMismatch(JNode node)
+        => node is JProperty property
+            ? new(property, $"key \"{property.Key}\" is found at position")
+            : new(node, "key not found at position");
+
+    private static JsonType GetType(JNode node) => ((IJsonType<JNode>) node).Type;
+}
